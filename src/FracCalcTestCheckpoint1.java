c893e1deba3da261fbff55275207102f5d35198c
rebeccasung <552090@my.cuhsd.org>
2022-11-08 13:43:44 -0800

    Passes checkpoint 1

--- src/FracCalcTestCheckpoint1.java
+++ src/FracCalcTestCheckpoint1.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import java.util.Scanner;
 
 // Checkpoint 1-only tests
 public class FracCalcTestCheckpoint1
@@ -6,4 +7,22 @@ public class FracCalcTestCheckpoint1
     @Test public void testCheckpoint1_1() {  FracCalcTestALL.assertForEarlyCheckpoints("6_5/8", "whole:6 numerator:5 denominator:8", "12_3/8", FracCalc.produceAnswer("5_3/4 + 6_5/8"));}
     @Test public void testCheckpoint1_2() {FracCalcTestALL.assertForEarlyCheckpoints("20", "whole:20 numerator:0 denominator:1", "-20_3/7", FracCalc.produceAnswer("-3/7 - 20"));}
     @Test public void testCheckpoint1_3() {FracCalcTestALL.assertForEarlyCheckpoints("27/21", "whole:0 numerator:27 denominator:21", "-33_2/7", FracCalc.produceAnswer("-32 - 27/21"));}
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a fractional expression (use an underscore as a space for mixed fractions):");
+        String fraction = input.nextLine();
+        System.out.println(produceAnswer(fraction));
+    }
+
+    public static String produceAnswer(String fraction)
+    {
+        Scanner fract = new Scanner(fraction);
+        fract.useDelimiter(" ");
+        String firstOperand = fract.next();
+        String operator = fract.next();
+        String secondOperand = fract.next();
+        return secondOperand;
+    }
+
 }
