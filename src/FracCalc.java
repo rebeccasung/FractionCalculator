14fadbec18cec86023646a527681ffaad94a8692
rebeccasung <552090@my.cuhsd.org>
2022-11-10 13:35:04 -0800

    Passes checkpoint 2

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 public class FracCalc {
 
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
+        Scanner input = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Enter a fractional expression (use an underscore as a space for mixed fractions):");
+            String fraction = input.nextLine();
+            System.out.println(produceAnswer(fraction));
+            if(fraction.equals("quit"))
+            {
+                break;
+            }
+        }
+
 
     }
     
@@ -14,11 +27,39 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
+    public static String produceAnswer(String fraction)
     { 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+        Scanner fract = new Scanner(fraction);
+
+        String whole = "";
+        String numerator = "";
+        String denominator = "";
+
+        fract.useDelimiter(" ");
+        String firstOperand = fract.next();
+        String operator = fract.next();
+        String secondOperand = fract.next();
+        Scanner secondOper = new Scanner(secondOperand);
+
+
+            if (secondOperand.contains("_") && secondOperand.contains("/")) {
+                secondOper.useDelimiter("[_/]");
+                whole = secondOper.next();
+                numerator = secondOper.next();
+                denominator = secondOper.next();
+
+            } else if (secondOperand.contains("/")) {
+                secondOper.useDelimiter("/");
+                whole = "0";
+                numerator = secondOper.next();
+                denominator = secondOper.next();
+            } else {
+                whole = secondOperand;
+                numerator = "0";
+                denominator = "1";
+            }
+        return "whole: " + whole + "\t" + "numerator: " + numerator + "\t" + "denominator: " + denominator;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
