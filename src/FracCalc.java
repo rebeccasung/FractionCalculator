1dcf4f7e53f2b3df67b7107a626de15df1761a2d
rebeccasung <552090@my.cuhsd.org>
2022-11-17 14:02:40 -0800

    Passes checkpoint 3

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class FracCalc {
 
@@ -5,6 +6,8 @@ public class FracCalc {
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
         Scanner input = new Scanner(System.in);
+
+        // keeps asking user for input until user enters quit
         while(true)
         {
             System.out.println("Enter a fractional expression (use an underscore as a space for mixed fractions):");
@@ -33,36 +36,152 @@ public class FracCalc {
     public static String produceAnswer(String fraction)
     { 
         // TODO: Implement this function to produce the solution to the input
+        // scanner to break up the user input into 3 parts (1st operand, operator, 2nd operand)
         Scanner fract = new Scanner(fraction);
 
-        String whole = "";
-        String numerator = "";
-        String denominator = "";
 
+        // initializing variables used later on
+        int whole = 0;
+        int numerator = 0;
+        int denominator = 1;
+        String finalFraction = "";
+
+        //using space delimiter to divide the input
         fract.useDelimiter(" ");
         String firstOperand = fract.next();
         String operator = fract.next();
         String secondOperand = fract.next();
+
+        //scanners to further divide each operand into numerators and denominators
         Scanner secondOper = new Scanner(secondOperand);
+        Scanner firstOper = new Scanner(firstOperand);
+
+        //firstOperand conversions to regular or improper fractions
+        if (firstOperand.contains("_") && firstOperand.contains("/")) {
+            firstOper.useDelimiter("[_/]");
+            whole = firstOper.nextInt();
+            numerator = firstOper.nextInt();
+            denominator = firstOper.nextInt();
+            // converting mixed fractions to improper fractions
+            if(whole<0){
+                firstOperand = (whole * denominator - numerator) +  "/" + denominator;
+            }
+            else
+            {
+                firstOperand = (whole * denominator + numerator) +  "/" + denominator;
+            }
+        } else if (firstOperand.contains("/")) {
+            firstOper.useDelimiter("/");
+            numerator = firstOper.nextInt();
+            denominator = firstOper.nextInt();
+        } else {
+            whole = Integer.parseInt(firstOperand);
+            numerator = 0;
+            denominator = 1;
+        }
 
 
+
+        // secondOperand conversions to a regular fraction
             if (secondOperand.contains("_") && secondOperand.contains("/")) {
                 secondOper.useDelimiter("[_/]");
-                whole = secondOper.next();
-                numerator = secondOper.next();
-                denominator = secondOper.next();
+                whole = secondOper.nextInt();
+                numerator = secondOper.nextInt();
+                denominator = secondOper.nextInt();
 
+                // converting mixed numbers to improper fractions
+                if(whole<0){
+                    secondOperand = (whole * denominator - numerator) +  "/" + denominator;
+                }
+                else
+                {
+                    secondOperand = (whole * denominator + numerator) +  "/" + denominator;
+                }
             } else if (secondOperand.contains("/")) {
                 secondOper.useDelimiter("/");
-                whole = "0";
-                numerator = secondOper.next();
-                denominator = secondOper.next();
+                numerator = secondOper.nextInt();
+                denominator = secondOper.nextInt();
             } else {
-                whole = secondOperand;
-                numerator = "0";
-                denominator = "1";
+                whole = Integer.parseInt(secondOperand);
+                numerator = 0;
+                denominator = 1;
             }
-        return "whole:" + whole + " " + "numerator:" + numerator + " " + "denominator:" + denominator;
+
+
+            // Scanners for numerators of denominators of fractions
+
+        //Scanner for first operand
+        Scanner first = new Scanner(firstOperand);
+            int num1 = 0;
+            int den1 = 0;
+
+            //fractions
+        if(firstOperand.contains("/")) {
+            first.useDelimiter("/");
+            num1 = first.nextInt();
+            den1 = first.nextInt();
+        }
+
+        //whole numbers
+        else {
+            num1 = first.nextInt();
+            den1 = 1;
+        }
+
+        //Scanner for second operand
+        Scanner second = new Scanner(secondOperand);
+        int num2 = 0;
+        int den2 = 0;
+
+        //fractions
+        if (secondOperand.contains("/"))
+        {
+            second.useDelimiter("/");
+            num2 = second.nextInt();
+            den2 = second.nextInt();
+        }
+
+        //whole numbers
+        else
+        {
+            num2 = second.nextInt();
+            den2 = 1;
+        }
+
+        // calculations
+
+            //addition
+            if (operator.equals("+"))
+            {
+                finalFraction = (num2*den1 + den2*num1) + "/" + (den2*den1);
+            }
+
+            // subtraction
+            else if (operator.equals("-"))
+            {
+                finalFraction = -(num2*den1 - den2*num1) + "/" + (den2*den1);
+            }
+
+            //multiplication
+            else if(operator.equals("*"))
+            {
+                finalFraction = (num1*num2) + "/" + (den1*den2);
+            }
+
+            // division
+            else if(operator.equals("/"))
+            {
+                finalFraction = (num1*den2) + "/" + (den1*num2);
+            }
+
+            //@throws InputMismatchException if user does not input a valid operator
+            else
+            {
+                throw new InputMismatchException("Not a valid operator!");
+            }
+
+
+        return finalFraction;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
